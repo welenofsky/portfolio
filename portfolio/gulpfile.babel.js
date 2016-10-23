0158433a7306469f705559803f1c55c132515ffe
Justin Welenofsky <welenofsky@gmail.com>
2016-10-23 16:04:50 -0700

    init

---
+++ portfolio/gulpfile.babel.js
@@ -0,0 +1,51 @@
+/**
+ * Created by justin on 10/23/16.
+ */
+import path from 'path';
+
+import gulp from 'gulp';
+
+
+// Load all gulp plugins automatically
+// and attach them to the `plugins` object
+import plugins from 'gulp-load-plugins';
+
+
+
+gulp.task('default', [
+	'scripts',
+	'styles:bootstrap'
+]);
+
+gulp.task('scripts', [
+	'scripts:jquery',
+	'scripts:bootstrap',
+	'scripts:tether'
+]);
+
+gulp.task('scripts:jquery', () =>
+	// place code for your default task here
+	gulp.src('node_modules/jquery/dist/jquery*(.min)+(.map|.js)')
+		.pipe(gulp.dest('static/js'))
+  		// .pipe(notify('Copied <%= file.relative %>!'));
+);
+
+gulp.task('scripts:bootstrap', () =>
+	// place code for your default task here
+	gulp.src('node_modules/bootstrap/dist/js/bootstrap*(.min)+(.map|.js)')
+		.pipe(gulp.dest('static/js'))
+  		// .pipe(notify('Copied <%= file.relative %>!'));
+);
+
+gulp.task('scripts:tether', () =>
+	// place code for your default task here
+	gulp.src('node_modules/tether/dist/js/tether*(.min).js')
+		.pipe(gulp.dest('static/js'))
+  		// .pipe(notify('Copied <%= file.relative %>!'));
+);
+
+gulp.task('styles:bootstrap', () =>
+	gulp.src('node_modules/bootstrap/dist/css/bootstrap-flex*(.min).css*(.map)')
+		.pipe(gulp.dest('static/css'))
+		//.pipe(plugins().notify('Bootstrap CSS copied successfully!'))
+);
---
+++ portfolio/portfolio/settings/local.py
@@ -0,0 +1,6 @@
+# settings/local.py
+# Overwrite all the namepsaces
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
